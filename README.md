6f38bfff9d07d2c05e99e69c338aaef89870b531
Alex Algarate <alex.algarate@gmail.com>
2023-03-08 22:37:51 +0100

    Making Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Correct-Elevation-Strava
+
+This script is designed to correct elevation data for Strava activities that have an elevation gain of 0. It uses Selenium to log into Strava and correct the elevation data for each activity in a specified database.
+
+## Requirements
+
+* Python 3.6 or higher
+* Chrome web browser
+* ChromeDriver (make sure to match the version with your Chrome browser)
+* The following Python packages: decouple, dotenv, sqlalchemy, selenium, webdriver_manager
--- correct_elevation.py
+++ correct_elevation.py
@@ -1,5 +1,4 @@
-import os
-
+from decouple import config
 from get_ids_database import get_ids_from_database
 
 from dotenv import load_dotenv
@@ -15,28 +14,58 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 load_dotenv()
 
-EMAIL = os.getenv('EMAIL')
-PASSWORD = os.getenv('PASSWORD')
-ENGINE = create_engine(os.getenv('ENGINE'))
+EMAIL = config('EMAIL')
+PASSWORD = config('PASSWORD')
+ENGINE = create_engine(config('ENGINE'))
 SECS = 10
-QUERY = '''
-    SELECT id
-    FROM "Summary_Strava"
-    WHERE sport_type = \'Run\' and
-    total_elevation_gain = \'0\' and
-    month = \'9\' and
-    year = \'2022\';
-'''
-ID_LIST = get_ids_from_database(QUERY, ENGINE)
+
+
+def get_activity_url(activity_id):
+    return f'https://www.strava.com/activities/{activity_id}'
+
+
+def login(driver) -> None:
+    driver.get('https://www.strava.com/login')
+
+    # Fill in the email and password fields
+    email_field = WebDriverWait(driver, SECS).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="email"]'))
+    )
+    email_field.send_keys(EMAIL)
+    password_field = WebDriverWait(driver, SECS).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="password"]'))
+    )
+    password_field.send_keys(PASSWORD)
+
+    # Click on the login button
+    login_button = driver.find_element(By.XPATH, '//*[@id="login-button"]')
+    login_button.click()
+
+
+def correct_elevation(driver) -> None:
+    # Click on the options button
+    options_button = WebDriverWait(driver, SECS).until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[3]/nav/div/button/div'))
+    )
+    options_button.click()
+
+    # Click on the correct elevation option
+    correct_elevation_option = WebDriverWait(driver, SECS).until(
+        EC.presence_of_element_located((By.XPATH, '(//*[@id="react-list-item"]/div/a)[2]'))
+    )
+    correct_elevation_option.click()
+
+    # Click on the button to correct the activity
+    correct_activity_button = WebDriverWait(driver, SECS).until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/reach-portal/div[2]/div/div/div/form/div[2]/button'))
+    )
+    correct_activity_button.click()
 
 
 def main():
-
-    # Set options
     options = Options()
     options.add_argument('--start-maximized')
     options.add_experimental_option("detach", True)
-
     # Start the driver
     with webdriver.Chrome(
         service=Service(
@@ -44,48 +73,19 @@ def main():
         ),
         options=options
     ) as driver:
-        # Load the login page
-        driver.get('https://www.strava.com/login')
-
-        # Fill in the email and password fields
-        email_field = WebDriverWait(driver, SECS).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="email"]'))
+        login(driver)
+        query = (
+            'SELECT id '
+            'FROM "Summary_Strava" '
+            'WHERE sport_type = \'Run\' and '
+            'total_elevation_gain = \'0\' and '
+            'year > \'2018\';'
         )
-        email_field.send_keys(EMAIL)
-        password_field = WebDriverWait(driver, SECS).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="password"]'))
-        )
-        password_field.send_keys(PASSWORD)
-
-        # Click on the login button
-        login_button = driver.find_element(By.XPATH, '//*[@id="login-button"]')
-        login_button.click()
-
-        # Loop through the activity IDs
-        for activity_id in ID_LIST:
-            # Load the activity page
-            activity_url = f'https://www.strava.com/activities/{activity_id}'
+        id_list = get_ids_from_database(query, ENGINE)
+        for activity_id in id_list:
+            activity_url = get_activity_url(activity_id)
             driver.get(activity_url)
-
-            # Click on the options button
-            options_button = WebDriverWait(driver, SECS).until(
-                EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[3]/nav/div/button/div'))
-            )
-            options_button.click()
-
-            # Click on the correct elevation option
-            correct_elevation_option = WebDriverWait(driver, SECS).until(
-                EC.presence_of_element_located((By.XPATH, '(//*[@id="react-list-item"]/div/a)[2]'))
-            )
-            correct_elevation_option.click()
-
-            # Click on the button to correct the activity
-            correct_activity_button = WebDriverWait(driver, SECS).until(
-                EC.presence_of_element_located((By.XPATH, '/html/body/reach-portal/div[2]/div/div/div/form/div[2]/button'))
-            )
-            correct_activity_button.click()
-
-        # Wait for a few seconds before closing the driver
+            correct_elevation(driver)
         driver.implicitly_wait(15)
 
 
